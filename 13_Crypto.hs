9a3f078092356176ae31f94c14a7d3dca0123612
kai-wang <simaow@outlook.com>
2017-09-02 22:11:11 +1000

    updates

--- 13_Crypto.hs
+++ 13_Crypto.hs
@@ -8,5 +8,6 @@ sha1 = hash
 hexSha3_512 :: ByteString -> String
 hexSha3_512 bs = show (hash bs :: Digest SHA3_512)
 
+main :: IO ()
 main = do
-    print (sha1 $ C.pack "test")
+  print $ sha1 $ C.pack "test"
