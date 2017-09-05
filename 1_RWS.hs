0657bed97ae9b48e19ba530eb7590b69585fae85
Wang <Kai.Wang@team.telstra.com>
2017-09-05 14:41:46 +1000

    update the format

--- 1_RWS.hs
+++ 1_RWS.hs
@@ -6,17 +6,17 @@ import Control.Monad.State
 import Control.Applicative
 
 data MyContext = MyContext {
-    foo :: String,
-    bar :: Int
+  foo :: String,
+  bar :: Int
 } deriving (Show)
 
 computation :: Reader MyContext (Maybe String)
 computation = do
-    n <- asks bar
-    x <- asks foo
-    if n > 0
-        then return (Just $ show n)
-        else return Nothing
+  n <- asks bar
+  x <- asks foo
+  if n > 0
+    then return (Just $ show n)
+    else return Nothing
 
 ex1 :: Maybe String
 ex1 = runReader computation $ MyContext "hello" 1
@@ -28,18 +28,18 @@ type MyWriter = Writer [Int] String
 
 example :: MyWriter
 example = do
-    tell [1..3]
-    tell [3..5]
-    return "foo"
+  tell [1..3]
+  tell [3..5]
+  return "foo"
 
 output :: (String, [Int])
 output = runWriter example
 
 test :: State Int Int
 test = do
-    put 3
-    modify (+1)
-    get
+  put 3
+  modify (+1)
+  get
 
 main :: IO()
 main = print $ evalState test 0
@@ -51,15 +51,15 @@ data Expr = Val Int | Add Expr Expr | Var String deriving (Show)
 
 eval :: Expr -> Eval Int
 eval ex = case ex of
-    Val n -> return n
-    Add x y -> do
-        a <- eval x
-        b <- eval y
-        return (a+b)
-    Var x -> do
-        env <- ask
-        val <- lift (lookup x env)
-        return val
+  Val n -> return n
+  Add x y -> do
+    a <- eval x
+    b <- eval y
+    return (a+b)
+  Var x -> do
+    env <- ask
+    val <- lift (lookup x env)
+    return val
 
 env :: Env
 env = [("x", 55), ("y", 5)]
@@ -79,32 +79,32 @@ type Program = [Instr]
 type VM a = ReaderT Program (WriterT Output (State Stack)) a
 
 newtype Comp a = Comp { unComp :: VM a }
-    deriving (Monad, MonadReader Program, MonadWriter Output, MonadState Stack)
+  deriving (Monad, MonadReader Program, MonadWriter Output, MonadState Stack)
 
 instance Functor Comp where
-    fmap = liftM
+  fmap = liftM
 
 instance Applicative Comp where
-    pure = return
-    (<*>) = ap
+  pure = return
+  (<*>) = ap
 
 data Instr = Push Int | Pop | Puts
 
 evalInstr :: Instr -> Comp ()
 evalInstr instr = case instr of 
-    Pop -> modify tail
-    Push n -> modify (n:)
-    Puts -> do
-        tos <- gets head
-        tell [tos]
+  Pop -> modify tail
+  Push n -> modify (n:)
+  Puts -> do
+    tos <- gets head
+    tell [tos]
 
 
 eval2 :: Comp ()
 eval2 = do
-    instr <- ask
-    case instr of
-        [] -> return ()
-        (i:is) -> evalInstr i >> local (const is) eval2
+  instr <- ask
+  case instr of
+    [] -> return ()
+    (i:is) -> evalInstr i >> local (const is) eval2
 
 execVM :: Program -> Output
 execVM = flip evalState [] . execWriterT . runReaderT (unComp eval2)
