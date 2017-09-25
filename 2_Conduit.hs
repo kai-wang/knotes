21e86b65c71c917b139c88c7cedee8ba06646be5
kai-wang <simaow@outlook.com>
2017-09-25 20:55:03 +1000

    add conduit

--- 2_Conduit.hs
+++ 2_Conduit.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
 
 import Data.Conduit
 import Control.Monad.Trans
 import qualified Data.Conduit.List as CL
+import qualified Data.Conduit.Combinators as CB
 
 source :: Source IO Int
 source = CL.sourceList [1..100]
@@ -23,5 +25,33 @@ conduit = do
 sink :: Sink String IO ()
 sink = CL.mapM_ putStrLn
 
+ex1 :: IO ()
+ex1 = do
+  putStrLn "List version:"
+  mapM_ print $ takeWhile (< 18) $ map (* 2) $ take 10 [1..]
+  putStrLn ""
+  putStrLn "Conduit version:"
+  runConduit
+     $ CB.yieldMany [1..]
+    .| CB.take 10
+    .| CB.map (* 2)
+    .| CB.takeWhile (< 18)
+    .| CB.mapM_ print
+
+ex2 :: IO ()
+ex2 = do 
+  putStrLn $ runConduitPure
+    $ CB.yieldMany [1..10 :: Int]
+   .| CB.map show
+   .| CB.unlines
+   .| CB.fold
+
+ex3 :: IO ()
+ex3 = do
+  print $ runConduitPure $ yield 1 .| await
+  print $ runConduitPure $ CB.yieldMany [] .| await
+  print $ runConduitPure $ return () .| await
+  print $ runConduitPure await
+   
 main :: IO ()
 main = source $$ conduit =$ sink
